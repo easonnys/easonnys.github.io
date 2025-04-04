2d8b0408ada49a4ccd4fd9cd793aba05457fd36d
easonnys <eason224005@gmail.com>
2025-04-04 13:20:11 +1100

    assingment1 complete

---
+++ week5/script.js
@@ -0,0 +1,33 @@
+console.log("Hello");
+console.log("How are you buddy");
+Variables;
+let myname = "Eason";
+console.log("Hello", myname);
+let id = "s4040595";
+console.log("My id is", id);
+
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(a, b, c);
+
+// boolean variable
+let isitsunday = false;
+const isitOART1013 = true;
+
+//object variabke
+const mystudentrecord = {
+  name: "Eason",
+  id: "s4040595",
+  hometown: "Malaysia",
+  isLocal: False,
+};
+
+console.log(mystudentrecord);
+console.log(mystudentrecord.name);
+
+//arrays
+let student1 = "Eason";
+let student2 = "Rohit";
+let student3 = "Lucy";
+let student4 = "Sarah";
