ddb9ff989fde7a218022d9df6bb0c9aeea8b33a7
easonnys <eason224005@gmail.com>
2025-04-17 13:54:20 +1000

    week6 complete

---
+++ week6/script.js
@@ -0,0 +1,17 @@
+const topHeading = document.querySelector("h1");
+console.log(topHeading);
+console.log(topHeading.textContent);
+topHeading.textContent = "this is a new heading";
+topHeading.style.color = "crimson";
+const allParas = document.querySelector("p");
+x``;
+
+const allParas = document.querySelector("p");
+console.log("allParas");
+for (let i = 0; i < allParas.length; i++) {
+  console.log(allParas[i].textContent);
+}
+
+const myFirstsub = document.querySelector("#first-subheading");
+console.log(myFirstsub);
+console.log(myFirstsub.textContent);
